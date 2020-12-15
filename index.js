cab7aa95c0f9a82ef1e0dc22c564d34fd26b1f1a
Ruan Andre Felssner <ruan_felssner@hotmail.com>
2020-12-15 20:31:41 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json('Seja bem vindo!');
+});
+
+app.listen(PORT);
