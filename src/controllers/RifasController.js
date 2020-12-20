069a274ff87f5379212c20a295371cb35f1b62e9
Ruan Andre Felssner <ruan_felssner@hotmail.com>
2020-12-20 01:23:26 -0300

    Cadastrar varios pontos nas rifas

--- src/controllers/RifasController.js
+++ src/controllers/RifasController.js
@@ -8,11 +8,31 @@ module.exports = {
         })
     },
     add(req, res, next){
-        const {name, qtdCotas, valorCotas} = req.body;
+        const {name, nomeRazao, cpfCnpj, email, instagram, whatsapp, tipoPagamento, tipoRifa, qtdCotasDisponiveis, qtdCotas, valorCotas, referenciaSorteio, responsavel, cidade, estado, fabricante, modelo, versao, motor, anoModelo, cor, avatar, descricao} = req.body;
         Rifa.create({
             name,
+            nomeRazao,
+            cpfCnpj,
+            email,
+            instagram,
+            whatsapp,
+            tipoPagamento,
+            tipoRifa,
+            qtdCotasDisponiveis,
             qtdCotas,
-            valorCotas
+            valorCotas,
+            referenciaSorteio,
+            responsavel,
+            cidade,
+            estado,
+            fabricante,
+            modelo,
+            versao,
+            motor,
+            anoModelo,
+            cor,
+            avatar,
+            descricao
         })
         .then((result) => {
             res.status(201).json(result) // retorna o id
